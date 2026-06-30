90e85f6a1e69c9adf2c9d485d870bb5fbc7de87d
Tom Chiasson <thom.chiasson@gmail.com>
2026-06-30 19:25:02 -0400

    Add b3b

--- data/packs.js
+++ data/packs.js
@@ -1439,4 +1439,75 @@ const packSeries = {
             }
         }
     },
+    "b3b": {
+        name: "Everyday Wonders (B3b)",
+        packTypes: {
+            regular: {
+                probability: 0.94711,
+                slots: [
+                    { oneDiamond: 1.0 },
+                    { oneDiamond: 1.0 },
+                    { oneDiamond: 1.0 },
+                    {
+                        crown: 0.0004,
+                        threeStar: 0.00222,
+                        twoStar: 0.005,
+                        oneStar: 0.02572,
+                        fourDiamond: 0.01667,
+                        threeDiamond: 0.05,
+                        twoDiamond: 0.89999,
+                    },
+                    {
+                        crown: 0.00160,
+                        threeStar: 0.00889,
+                        twoStar: 0.02,
+                        oneStar: 0.10286,
+                        fourDiamond: 0.06667,
+                        threeDiamond: 0.2000,
+                        twoDiamond: 0.59998,
+                    },
+                ],
+            },
+            rare: {
+                probability: 0.0005,
+                slots: [
+                    { crown: 0.09523, threeStar: 0.04761, twoStar: 0.57142, oneStar: 0.28571 },
+                    { crown: 0.03921, threeStar: 0.03921, twoStar: 0.45098, oneStar: 0.47058 },
+                    { crown: 0.03921, threeStar: 0.03921, twoStar: 0.45098, oneStar: 0.47058 },
+                    { crown: 0.03921, threeStar: 0.03921, twoStar: 0.45098, oneStar: 0.47058 },
+                    { crown: 0.03921, threeStar: 0.03921, twoStar: 0.45098, oneStar: 0.47058 },
+                ],
+            },
+            shiny: {
+                probability: 0.05238,
+                slots: [
+                    { oneDiamond: 1.0 },
+                    { oneDiamond: 1.0 },
+                    { oneDiamond: 1.0 },
+                    {
+                        crown: 0.0004,
+                        threeStar: 0.00222,
+                        twoStar: 0.005,
+                        oneStar: 0.02572,
+                        fourDiamond: 0.01667,
+                        threeDiamond: 0.05,
+                        twoDiamond: 0.89999,
+                    },
+                    {
+                        crown: 0.0016,
+                        threeStar: 0.00889,
+                        twoStar: 0.02,
+                        oneStar: 0.10286,
+                        fourDiamond: 0.06667,
+                        threeDiamond: 0.2000,
+                        twoDiamond: 0.59998,
+                    },
+                    {
+                        shinyDouble: 0.31820,
+                        shiny: 0.68180,
+                    }
+                ]
+            }
+        }
+    },
 };
---
+++ data/sets/b3b.js
@@ -0,0 +1,638 @@
+const b3b = {
+  "1": {
+    "number": "1",
+    "name": "Caterpie",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "2": {
+    "number": "2",
+    "name": "Metapod",
+    "rarity": "twoDiamond",
+    "series": "b3b"
+  },
+  "3": {
+    "number": "3",
+    "name": "Butterfree",
+    "rarity": "threeDiamond",
+    "series": "b3b"
+  },
+  "4": {
+    "number": "4",
+    "name": "Cacnea",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "5": {
+    "number": "5",
+    "name": "Tropius",
+    "rarity": "twoDiamond",
+    "series": "b3b"
+  },
+  "6": {
+    "number": "6",
+    "name": "Petilil",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "7": {
+    "number": "7",
+    "name": "Hisuian Lilligant",
+    "rarity": "twoDiamond",
+    "series": "b3b"
+  },
+  "8": {
+    "number": "8",
+    "name": "Vulpix",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "9": {
+    "number": "9",
+    "name": "Ninetales",
+    "rarity": "twoDiamond",
+    "series": "b3b"
+  },
+  "10": {
+    "number": "10",
+    "name": "Growlithe",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "11": {
+    "number": "11",
+    "name": "Psyduck",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "12": {
+    "number": "12",
+    "name": "Goldeen",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "13": {
+    "number": "13",
+    "name": "Seaking",
+    "rarity": "twoDiamond",
+    "series": "b3b"
+  },
+  "14": {
+    "number": "14",
+    "name": "Feebas",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "15": {
+    "number": "15",
+    "name": "Milotic ex",
+    "rarity": "fourDiamond",
+    "series": "b3b"
+  },
+  "16": {
+    "number": "16",
+    "name": "Snorunt",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "17": {
+    "number": "17",
+    "name": "Froslass",
+    "rarity": "threeDiamond",
+    "series": "b3b"
+  },
+  "18": {
+    "number": "18",
+    "name": "Luvdisc",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "19": {
+    "number": "19",
+    "name": "Piplup",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "20": {
+    "number": "20",
+    "name": "Iron Bundle",
+    "rarity": "twoDiamond",
+    "series": "b3b"
+  },
+  "21": {
+    "number": "21",
+    "name": "Pikachu",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "22": {
+    "number": "22",
+    "name": "Raichu",
+    "rarity": "twoDiamond",
+    "series": "b3b"
+  },
+  "23": {
+    "number": "23",
+    "name": "Emolga",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "24": {
+    "number": "24",
+    "name": "Dedenne ex",
+    "rarity": "fourDiamond",
+    "series": "b3b"
+  },
+  "25": {
+    "number": "25",
+    "name": "Yamper",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "26": {
+    "number": "26",
+    "name": "Slowpoke",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "27": {
+    "number": "27",
+    "name": "Slowbro",
+    "rarity": "twoDiamond",
+    "series": "b3b"
+  },
+  "28": {
+    "number": "28",
+    "name": "Munna",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "29": {
+    "number": "29",
+    "name": "Musharna",
+    "rarity": "twoDiamond",
+    "series": "b3b"
+  },
+  "30": {
+    "number": "30",
+    "name": "Sylveon",
+    "rarity": "threeDiamond",
+    "series": "b3b"
+  },
+  "31": {
+    "number": "31",
+    "name": "Carbink",
+    "rarity": "twoDiamond",
+    "series": "b3b"
+  },
+  "32": {
+    "number": "32",
+    "name": "Mega Diancie ex",
+    "rarity": "fourDiamond",
+    "series": "b3b"
+  },
+  "33": {
+    "number": "33",
+    "name": "Enamorus",
+    "rarity": "threeDiamond",
+    "series": "b3b"
+  },
+  "34": {
+    "number": "34",
+    "name": "Fidough",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "35": {
+    "number": "35",
+    "name": "Flutter Mane",
+    "rarity": "twoDiamond",
+    "series": "b3b"
+  },
+  "36": {
+    "number": "36",
+    "name": "Wooper",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "37": {
+    "number": "37",
+    "name": "Quagsire",
+    "rarity": "twoDiamond",
+    "series": "b3b"
+  },
+  "38": {
+    "number": "38",
+    "name": "Rockruff",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "39": {
+    "number": "39",
+    "name": "Sandygast",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "40": {
+    "number": "40",
+    "name": "Palossand",
+    "rarity": "twoDiamond",
+    "series": "b3b"
+  },
+  "41": {
+    "number": "41",
+    "name": "Mega Sableye ex",
+    "rarity": "fourDiamond",
+    "series": "b3b"
+  },
+  "42": {
+    "number": "42",
+    "name": "Cacturne",
+    "rarity": "twoDiamond",
+    "series": "b3b"
+  },
+  "43": {
+    "number": "43",
+    "name": "Spiritomb",
+    "rarity": "twoDiamond",
+    "series": "b3b"
+  },
+  "44": {
+    "number": "44",
+    "name": "Scraggy",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "45": {
+    "number": "45",
+    "name": "Scrafty",
+    "rarity": "twoDiamond",
+    "series": "b3b"
+  },
+  "46": {
+    "number": "46",
+    "name": "Mareanie",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "47": {
+    "number": "47",
+    "name": "Toxapex",
+    "rarity": "twoDiamond",
+    "series": "b3b"
+  },
+  "48": {
+    "number": "48",
+    "name": "Goomy",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "49": {
+    "number": "49",
+    "name": "Hisuian Sliggoo",
+    "rarity": "twoDiamond",
+    "series": "b3b"
+  },
+  "50": {
+    "number": "50",
+    "name": "Hisuian Goodra",
+    "rarity": "threeDiamond",
+    "series": "b3b"
+  },
+  "51": {
+    "number": "51",
+    "name": "Jigglypuff",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "52": {
+    "number": "52",
+    "name": "Wigglytuff",
+    "rarity": "twoDiamond",
+    "series": "b3b"
+  },
+  "53": {
+    "number": "53",
+    "name": "Eevee",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "54": {
+    "number": "54",
+    "name": "Munchlax",
+    "rarity": "threeDiamond",
+    "series": "b3b"
+  },
+  "55": {
+    "number": "55",
+    "name": "Snorlax",
+    "rarity": "threeDiamond",
+    "series": "b3b"
+  },
+  "56": {
+    "number": "56",
+    "name": "Teddiursa",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "57": {
+    "number": "57",
+    "name": "Ursaring",
+    "rarity": "twoDiamond",
+    "series": "b3b"
+  },
+  "58": {
+    "number": "58",
+    "name": "Ursaluna",
+    "rarity": "threeDiamond",
+    "series": "b3b"
+  },
+  "59": {
+    "number": "59",
+    "name": "Hisuian Zorua",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "60": {
+    "number": "60",
+    "name": "Hisuian Zoroark ex",
+    "rarity": "fourDiamond",
+    "series": "b3b"
+  },
+  "61": {
+    "number": "61",
+    "name": "Furfrou",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "62": {
+    "number": "62",
+    "name": "Skwovet",
+    "rarity": "oneDiamond",
+    "series": "b3b"
+  },
+  "63": {
+    "number": "63",
+    "name": "Greedent",
+    "rarity": "twoDiamond",
+    "series": "b3b"
+  },
+  "64": {
+    "number": "64",
+    "name": "Small Balloon",
+    "rarity": "twoDiamond",
+    "series": "b3b"
+  },
+  "65": {
+    "number": "65",
+    "name": "Elegant Cape",
+    "rarity": "twoDiamond",
+    "series": "b3b"
+  },
+  "66": {
+    "number": "66",
+    "name": "Elesa",
+    "rarity": "twoDiamond",
+    "series": "b3b"
+  },
+  "67": {
+    "number": "67",
+    "name": "Puppy-Loving Girl",
+    "rarity": "twoDiamond",
+    "series": "b3b"
+  },
+  "68": {
+    "number": "68",
+    "name": "Wallace",
+    "rarity": "twoDiamond",
+    "series": "b3b"
+  },
+  "69": {
+    "number": "69",
+    "name": "Kid&#039;s Room",
+    "rarity": "twoDiamond",
+    "series": "b3b"
+  },
+  "70": {
+    "number": "70",
+    "name": "Vulpix",
+    "rarity": "oneStar",
+    "series": "b3b"
+  },
+  "71": {
+    "number": "71",
+    "name": "Piplup",
+    "rarity": "oneStar",
+    "series": "b3b"
+  },
+  "72": {
+    "number": "72",
+    "name": "Pikachu",
+    "rarity": "oneStar",
+    "series": "b3b"
+  },
+  "73": {
+    "number": "73",
+    "name": "Sylveon",
+    "rarity": "oneStar",
+    "series": "b3b"
+  },
+  "74": {
+    "number": "74",
+    "name": "Mareanie",
+    "rarity": "oneStar",
+    "series": "b3b"
+  },
+  "75": {
+    "number": "75",
+    "name": "Jigglypuff",
+    "rarity": "oneStar",
+    "series": "b3b"
+  },
+  "76": {
+    "number": "76",
+    "name": "Snorlax",
+    "rarity": "oneStar",
+    "series": "b3b"
+  },
+  "77": {
+    "number": "77",
+    "name": "Greedent",
+    "rarity": "oneStar",
+    "series": "b3b"
+  },
+  "78": {
+    "number": "78",
+    "name": "Milotic ex",
+    "rarity": "twoStar",
+    "series": "b3b"
+  },
+  "79": {
+    "number": "79",
+    "name": "Dedenne ex",
+    "rarity": "twoStar",
+    "series": "b3b"
+  },
+  "80": {
+    "number": "80",
+    "name": "Mega Diancie ex",
+    "rarity": "twoStar",
+    "series": "b3b"
+  },
+  "81": {
+    "number": "81",
+    "name": "Mega Sableye ex",
+    "rarity": "twoStar",
+    "series": "b3b"
+  },
+  "82": {
+    "number": "82",
+    "name": "Hisuian Zoroark ex",
+    "rarity": "twoStar",
+    "series": "b3b"
+  },
+  "83": {
+    "number": "83",
+    "name": "Elesa",
+    "rarity": "twoStar",
+    "series": "b3b"
+  },
+  "84": {
+    "number": "84",
+    "name": "Puppy-Loving Girl",
+    "rarity": "twoStar",
+    "series": "b3b"
+  },
+  "85": {
+    "number": "85",
+    "name": "Wallace",
+    "rarity": "twoStar",
+    "series": "b3b"
+  },
+  "86": {
+    "number": "86",
+    "name": "Milotic ex",
+    "rarity": "twoStar",
+    "series": "b3b"
+  },
+  "87": {
+    "number": "87",
+    "name": "Mega Diancie ex",
+    "rarity": "twoStar",
+    "series": "b3b"
+  },
+  "88": {
+    "number": "88",
+    "name": "Mega Sableye ex",
+    "rarity": "twoStar",
+    "series": "b3b"
+  },
+  "89": {
+    "number": "89",
+    "name": "Hisuian Zoroark ex",
+    "rarity": "twoStar",
+    "series": "b3b"
+  },
+  "90": {
+    "number": "90",
+    "name": "Dedenne ex",
+    "rarity": "threeStar",
+    "series": "b3b"
+  },
+  "91": {
+    "number": "91",
+    "name": "Caterpie",
+    "rarity": "oneStar",
+    "series": "b3b"
+  },
+  "92": {
+    "number": "92",
+    "name": "Metapod",
+    "rarity": "oneStar",
+    "series": "b3b"
+  },
+  "93": {
+    "number": "93",
+    "name": "Butterfree",
+    "rarity": "oneStar",
+    "series": "b3b"
+  },
+  "94": {
+    "number": "94",
+    "name": "Mareep",
+    "rarity": "oneStar",
+    "series": "b3b"
+  },
+  "95": {
+    "number": "95",
+    "name": "Flaaffy",
+    "rarity": "oneStar",
+    "series": "b3b"
+  },
+  "96": {
+    "number": "96",
+    "name": "Ampharos",
+    "rarity": "oneStar",
+    "series": "b3b"
+  },
+  "97": {
+    "number": "97",
+    "name": "Slowbro",
+    "rarity": "oneStar",
+    "series": "b3b"
+  },
+  "98": {
+    "number": "98",
+    "name": "Kangaskhan",
+    "rarity": "oneStar",
+    "series": "b3b"
+  },
+  "99": {
+    "number": "99",
+    "name": "Ditto",
+    "rarity": "oneStar",
+    "series": "b3b"
+  },
+  "100": {
+    "number": "100",
+    "name": "Snorlax",
+    "rarity": "oneStar",
+    "series": "b3b"
+  },
+  "101": {
+    "number": "101",
+    "name": "Mega Gyarados ex",
+    "rarity": "twoStar",
+    "series": "b3b"
+  },
+  "102": {
+    "number": "102",
+    "name": "Vaporeon ex",
+    "rarity": "twoStar",
+    "series": "b3b"
+  },
+  "103": {
+    "number": "103",
+    "name": "Mega Ampharos ex",
+    "rarity": "twoStar",
+    "series": "b3b"
+  },
+  "104": {
+    "number": "104",
+    "name": "Indeedee ex",
+    "rarity": "twoStar",
+    "series": "b3b"
+  },
+  "105": {
+    "number": "105",
+    "name": "Munchlax",
+    "rarity": "crown",
+    "series": "b3b"
+  },
+  "106": {
+    "number": "106",
+    "name": "Small Balloon",
+    "rarity": "crown",
+    "series": "b3b"
+  }
+};
